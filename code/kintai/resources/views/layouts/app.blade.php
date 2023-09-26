015cf199d2c6912344d36ccd212c80b5f3d51d03
tanaka-tsu <support@0kara.jp>
2023-09-26 19:02:53 +0900

    Bootstrap残り

--- code/kintai/resources/views/layouts/app.blade.php
+++ code/kintai/resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
