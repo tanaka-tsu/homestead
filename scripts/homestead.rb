b93e418946175ee5e0f3acf1261e258635ca139d
tanaka-tsu <support@0kara.jp>
2024-02-27 17:49:39 +0900

    Update homestead.rb

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -25,7 +25,7 @@ class Homestead
 
     # Configure A Private Network IP
     if settings['ip'] != 'autonetwork'
-      config.vm.network :private_network, ip: settings['ip'] ||= '192.168.56.56'
+      config.vm.network :private_network, ip: settings['ip'] ||= '192.168.56.30'
     else
       config.vm.network :private_network, ip: '0.0.0.0', auto_network: true
     end
