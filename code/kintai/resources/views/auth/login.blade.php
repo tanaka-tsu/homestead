058ecda0584b4ac485993f79553a3e57fa460c85
TakiMi0o0 <takimiworks@gmail.com>
2024-08-08 17:05:16 +0900

    エラーメッセージの日本語化

--- code/kintai/resources/views/auth/login.blade.php
+++ code/kintai/resources/views/auth/login.blade.php
@@ -18,6 +18,7 @@
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
+                                    <br>
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -32,6 +33,7 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
+                                    <br>
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
